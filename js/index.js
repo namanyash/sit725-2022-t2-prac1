1b74562a00b1029b9b76e905567a0f907dccf654
namanyash <naman_yash@yahoo.in>
2022-07-26 19:17:53 +1000

    code optimized

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 function getRandomText() {
     var randomText = ["text 1", "hello user", "hello test 3", "oops error! text", "random-text"]
-    return randomText[Math.floor(Math.random()*5)];
+    return randomText[Math.floor(Math.random()*randomText.length)];
 }
 
 function replaceText(){
